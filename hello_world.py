b9d996954426c248afed189944009a33f3c5ba1e
everyend1717 <ruhulu1717@kakao.com>
2024-09-11 15:45:14 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
-def print_hello():
-    print("Hello, World!")
+def add(x,y):
+    return x+y
+
 
 def print_KIT():
     print("Kumoh National Institute of Technology.")
 
 if __name__ == "__main__":
     print_KIT()
-    print_hello()
+    add()
